30ad8fc3d211fbe3ac7cae229211fdab5bdcd9cf
Fred Morrison <fpmorrison@verizon.net>
2020-05-04 21:19:46 -0400

    convert product retrieval to REST API call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 //
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { WelcomeComponent } from './home/welcome.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+;
 
 const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
@@ -32,6 +34,7 @@ const routeOptions: ExtraOptions = {
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     RouterModule.forRoot(routes, routeOptions),
   ],
   bootstrap: [AppComponent],
