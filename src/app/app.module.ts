956b6834b4038ca2fed999073541a3031a3bd036
Fred Morrison <fpmorrison@verizon.net>
2020-05-13 13:16:01 -0400

    create separate modules, including route modules for features and shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 //
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+//
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductModule } from './products/product.module';
-// Warning: additional routes are defined within ProductModule
-const routes: Routes = [
-  { path: 'welcome', component: WelcomeComponent },
-  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-  { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
-];
-
-const routeOptions: ExtraOptions = {
-  enableTracing: true,
-};
 
 @NgModule({
   declarations: [AppComponent, WelcomeComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(routes, routeOptions),
-    ProductModule,
+    ProductModule, // includes child routes
+    AppRoutingModule, // includes top-level routes, including wildcard route
   ],
 
   bootstrap: [AppComponent],
--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,23 +1,13 @@
 // Product Feature Module
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
 //
 import { SharedModule } from '../shared/shared.module';
 //
+import { ProductRoutingModule } from './product-routing.module';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { ProductDetailGuard } from './product-detail.guard';
-
-const childRoutes: Routes = [
-  { path: 'products', component: ProductListComponent },
-  {
-    path: 'products/:id',
-    canActivate: [ProductDetailGuard],
-    component: ProductDetailComponent,
-  },
-];
 
 @NgModule({
   declarations: [
@@ -25,6 +15,6 @@ const childRoutes: Routes = [
     ProductDetailComponent,
     ConvertToSpacesPipe,
   ],
-  imports: [CommonModule, RouterModule.forChild(childRoutes), SharedModule],
+  imports: [CommonModule, SharedModule, ProductRoutingModule],
 })
 export class ProductModule {}
