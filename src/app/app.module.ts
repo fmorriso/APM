15ae420294f161b9462a8ad038eff5eac0156c8e
Fred Morrison <fpmorrison@verizon.net>
2020-05-13 11:39:54 -0400

    create separate Shared and Product modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 //
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
-import { ProductDetailGuard } from './products/product-detail.guard';
-
+import { ProductModule } from './products/product.module';
+// Warning: additional routes are defined within ProductModule
 const routes: Routes = [
-  { path: 'products', component: ProductListComponent },
-  {
-    path: 'products/:id',
-    canActivate: [ProductDetailGuard],
-    component: ProductDetailComponent,
-    data: { isValidId: true },
-  },
   { path: 'welcome', component: WelcomeComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
@@ -30,20 +18,14 @@ const routeOptions: ExtraOptions = {
 };
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    WelcomeComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
-  ],
+  declarations: [AppComponent, WelcomeComponent],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot(routes, routeOptions),
+    ProductModule,
   ],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
